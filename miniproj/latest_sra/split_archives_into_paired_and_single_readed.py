23c5cee471fc5a3ca85d3952609b047bea113603
Finkonn <oromas01@gmail.com>
2026-04-27 16:52:44 +0300

    SRA archives final, ZOVER astroviruses table

--- miniproj/latest_sra/split_archives_into_paired_and_single_readed.py
+++ miniproj/latest_sra/split_archives_into_paired_and_single_readed.py
@@ -1,14 +1,14 @@
 import pandas as pd
 
-input_file = "sra_filtered_no_16S.csv"   
+input_file = "our_SRA_with_astro_from_ZOVER.csv"   
 
 df = pd.read_csv(input_file, sep=None, engine="python")
 
 paired = df[df["LibraryLayout"] == "PAIRED"]["Run"]
 single = df[df["LibraryLayout"] == "SINGLE"]["Run"]
 
-paired.to_csv("paired_runs.txt", index=False, header=False)
-single.to_csv("single_runs.txt", index=False, header=False)
+paired.to_csv("paired_runs_astro_ZOVER.txt", index=False, header=False)
+single.to_csv("single_runs_astro_ZOVER.txt", index=False, header=False)
 
 print(f"PAIRED: {len(paired)}")
 print(f"SINGLE: {len(single)}")
