4b6e87656cfc826b2fdfb4a3059dc88d56f22ee6
Finkonn <oromas01@gmail.com>
2026-06-24 16:26:16 +0300

    Add only bats astro stats and paired/single runs list

--- miniproj/latest_sra/split_archives_into_paired_and_single_readed.py
+++ miniproj/latest_sra/split_archives_into_paired_and_single_readed.py
@@ -1,14 +1,14 @@
 import pandas as pd
 
-input_file = "our_SRA_with_astro_from_ZOVER.csv"   
+input_file = "SRA_archives_ultra_final.xlsx"   
 
-df = pd.read_csv(input_file, sep=None, engine="python")
+df = pd.read_excel(input_file)
 
 paired = df[df["LibraryLayout"] == "PAIRED"]["Run"]
 single = df[df["LibraryLayout"] == "SINGLE"]["Run"]
 
-paired.to_csv("paired_runs_astro_ZOVER.txt", index=False, header=False)
-single.to_csv("single_runs_astro_ZOVER.txt", index=False, header=False)
+paired.to_csv("paired_runs_.txt", index=False, header=False)
+single.to_csv("single_runs_.txt", index=False, header=False)
 
 print(f"PAIRED: {len(paired)}")
 print(f"SINGLE: {len(single)}")
